c0f5703de0de89874df398074c769f0e5db0ec99
yamamotoyuki <wabisabiwasabin@gmail.com>
2018-01-13 22:42:49 +0900

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import spark.Spark.get
+import todolist.Task
+
+fun main(args: Array<String>) {
+    val objectMapper = ObjectMapper().registerKotlinModule()
+    get("/hello") { request, response ->
+        val name: String? = request.queryParams("name")
+        "Hello ${name ?: "world"}"
+    }
+
+    get("/task") { request, response ->
+        val task = listOf(Task(0, "お金を振り込む", false), Task(1, "電気を消す", false))
+        objectMapper.writeValueAsString(task)
+    }
+}
